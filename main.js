ab37c3fda6325a43e7227dde75ec6183a3cdce27
Sebqstin <110260631+Sebqstin@users.noreply.github.com>
2023-10-25 18:09:12 -0300

    s

--- main.js
+++ main.js
@@ -9,3 +9,40 @@ function TocaSomClap(){
 }
 
 document.querySelector('.tecla_clap').onclick=TocaSomClap;
+
+function TocaSomTim(){
+    document.querySelector('#som_tecla_tim').play()
+}
+
+document.querySelector('.tecla_tim').onclick=TocaSomTim;
+
+
+
+function TocaSomPuff(){
+    document.querySelector('#som_tecla_Puff').play()
+}
+
+document.querySelector('.tecla_Puff').onclick=TocaSomPuff;
+
+
+
+
+
+
+function TocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play()
+}
+
+document.querySelector('.tecla_splash').onclick=TocaSomSplash;
+
+
+
+
+
+
+function TocaSomTim(){
+    document.querySelector('#som_tecla_tim').play()
+}
+
+document.querySelector('.tecla_tim').onclick=TocaSomTim;
+
